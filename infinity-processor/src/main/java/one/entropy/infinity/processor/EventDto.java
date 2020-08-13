231b8fb63c950e29efabefa2a0951829ccd8ea83
Marat Gubaidullin <marat.gubaidullin@gmail.com>
2020-08-13 15:36:56 -0400

    Fixed problem with connection to Cassandra and Kafka

--- infinity-processor/src/main/java/one/entropy/infinity/processor/EventDto.java
+++ infinity-processor/src/main/java/one/entropy/infinity/processor/EventDto.java
@@ -1,4 +1,4 @@
-package one.entropy.infinity.processor.dto;
+package one.entropy.infinity.processor;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
--- infinity-processor/src/main/java/one/entropy/infinity/processor/dto/EventDtoDeserializer.java
+++
@@ -1,9 +0,0 @@
-package one.entropy.infinity.processor.dto;
-
-import io.quarkus.kafka.client.serialization.ObjectMapperDeserializer;
-
-public class EventDtoDeserializer extends ObjectMapperDeserializer<EventDto> {
-    public EventDtoDeserializer() {
-        super(EventDto.class);
-    }
-}
