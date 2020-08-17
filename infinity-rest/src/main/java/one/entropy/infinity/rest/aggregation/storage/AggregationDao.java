2ae939e15a756e3f98a3603d670f9ba2b15ec07b
Marat Gubaidullin <marat.gubaidullin@gmail.com>
2020-08-17 10:53:33 -0400

    Fixed get aggregations in REST api

--- infinity-rest/src/main/java/one/entropy/infinity/rest/aggregation/storage/AggregationDao.java
+++ infinity-rest/src/main/java/one/entropy/infinity/rest/aggregation/storage/AggregationDao.java
@@ -7,7 +7,7 @@ import com.datastax.oss.quarkus.runtime.api.reactive.mapper.MutinyMappedReactive
 @Dao
 public interface AggregationDao {
     @Select(customWhereClause =
-            "event_group = :eventGroup and event_type = :eventType and horizon = :horizon and period < :period ")
+            "event_group = :eventGroup and event_type = :eventType and horizon = :horizon and period <= :period ")
     MutinyMappedReactiveResultSet<Aggregation> findAggregations(
             String eventGroup, String eventType, String horizon, String period);
 
